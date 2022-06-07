16cc31e8c1920eca001de20c2cbc4acb35f8a10c
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-06 22:44:06 -0300

    created Navbar and Footer components

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -8,16 +8,23 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      {/* Navbar */}
+      <Navbar/>
         <div className="container">
           <Routes>
             <Route path="/" element={ <Home/> }/>
             <Route path="/about" element={ <About/> }/>
           </Routes>
         </div>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ miniblog/src/components/Navbar.js
@@ -0,0 +1,29 @@
+// Styling
+import styles from "./Navbar.module.css";
+
+// NavLink import
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink to="/">
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
