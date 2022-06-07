fb02ebb46b162254e01e2ec54c29be25f2514fd6
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-06 22:22:26 -0300

    firebase config created

---
+++ miniblog/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQG1YvLKnQM9BQsCLCRAaLNIL2HXxIgck",
+  authDomain: "miniblog-41d4d.firebaseapp.com",
+  projectId: "miniblog-41d4d",
+  storageBucket: "miniblog-41d4d.appspot.com",
+  messagingSenderId: "127366670099",
+  appId: "1:127366670099:web:a347d070f21e2d6ddfdce6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Getting the firestore
+const db = getFirestore(app)
+
+// Exporting
+export { db }
