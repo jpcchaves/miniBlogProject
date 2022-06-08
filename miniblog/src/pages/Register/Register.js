e98ab9510982701dbba3b58f9a3c060fdd638d66
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-07 22:06:45 -0300

    global form, buttons and error msg styles added

--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -1,11 +1,55 @@
-import styles from "./Register.module.css"
+// CSS
+import styles from "./Register.module.css";
+
+// Hooks
+import { useState, useEffect } from "react";
 
 const Register = () => {
   return (
     <div>
-      <h2>Registro</h2>
+      <h1>Realize seu cadastro</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário..."
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Email do usuário..."
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha...."
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme asua senha...."
+          />
+        </label>
+        <button className="btn">Cadastrar</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
