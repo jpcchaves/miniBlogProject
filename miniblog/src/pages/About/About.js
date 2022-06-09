a4556e2b138dc2a89b1b6c395cf1cef5b9efb10b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-08 22:43:17 -0300

    about page updated

--- miniblog/src/pages/About/About.js
+++ miniblog/src/pages/About/About.js
@@ -1,10 +1,22 @@
 // CSS Modules import
-import styles from "./About.module.css"
+import styles from "./About.module.css";
+
+// Link
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com ReactJS no Front-end e
+        Firebase no Back-end
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
     </div>
   );
 };
