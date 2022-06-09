b5a8525e1f2c4c148f91a280b9a550e8dea2d4ee
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-08 22:19:57 -0300

    AuthContext created

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -4,6 +4,14 @@ import "./App.css";
 // React Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Auth firebase
+import { onAuthStateChanged } from "firebase/auth";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+// Context
 import { AuthProvider } from "./context/AuthContext";
 
 // Pages
@@ -17,9 +25,24 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           {/* Navbar */}
           <Navbar />
