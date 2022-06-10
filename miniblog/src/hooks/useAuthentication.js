c9559094d4299f444b1f14bcb2267836de4b1038
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-09 21:54:48 -0300

    login authentication added

--- miniblog/src/hooks/useAuthentication.js
+++ miniblog/src/hooks/useAuthentication.js
@@ -77,6 +77,30 @@ export const useAuthentication = () => {
     signOut(auth);
   };
 
+  // Login - sign in
+  const login = async (data) => {
+    // avoiding memory leak
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false)
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro. Por favor, tente mais tarde.";
+      }
+      setError(systemErrorMessage);
+      setLoading(false)
+    }
+  };
+
   //   Avoiding memory leak
   useEffect(() => {
     return () => setCancelled(true);
@@ -88,5 +112,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login
   };
 };
--- miniblog/src/pages/Login/Login.js
+++ miniblog/src/pages/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const { createUser, error: authError, loading } = useAuthentication();
+  const { login, error: authError, loading } = useAuthentication();
 
   // handle submit
   const handleSubmit = async (e) => {
@@ -25,7 +25,7 @@ const Login = () => {
       password: password,
     };
 
-    const res = await createUser(user);
+    const res = await login(user);
 
     console.log(res);
   };
