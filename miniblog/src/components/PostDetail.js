d17c30bbcd675f267804a3632afdae3a4126f74d
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-11 10:38:18 -0300

    postDetails styles updated

--- miniblog/src/components/PostDetail.js
+++ miniblog/src/components/PostDetail.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 const PostDetail = ({ post }) => {
   return (
-    <div>
+    <div class={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p>{post.createdBy}</p>
-      <div>
+      <p className={styles.createdby}>{post.createdBy}</p>
+      <div className={styles.tags}>
         {post.tagsArray.map((tag) => (
           <p key={tag}>
             <span>#</span>
@@ -16,7 +16,9 @@ const PostDetail = ({ post }) => {
           </p>
         ))}
       </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler post</Link>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler post
+      </Link>
     </div>
   );
 };
