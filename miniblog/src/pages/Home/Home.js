641ec296c1ce56db8d4c8a0ee874ad559e37725b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-06-11 10:22:38 -0300

    imported fetchDocuments hooks to home

--- miniblog/src/pages/Home/Home.js
+++ miniblog/src/pages/Home/Home.js
@@ -4,12 +4,13 @@ import styles from "./Home.module.css";
 // Hooks
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 // components
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts, setPosts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,11 +28,14 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts....</h1>
+        {loading && <p>Carregando.....</p>}
+        {posts && posts.map((post) => <h3>{post.title}</h3>)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
           </div>
         )}
       </div>
